b2ce11dc8fac974af42b1d241373b4780466c4e5
jiangzj <jiangzj1008@outlook.com>
2017-07-30 03:01:01 +0800

    初始化

---
+++ gegame/ge_game.js
@@ -0,0 +1,112 @@
+class GeGame {
+    constructor(fps, images, runCallback) {
+        window.fps = fps
+        this.images = images
+        this.runCallback = runCallback
+        //
+        this.scene = null
+        this.actions = {}
+        this.keydowns = {}
+        this.canvas = document.querySelector('#id-canvas')
+        this.context = this.canvas.getContext('2d')
+        // events
+        var self = this
+        window.addEventListener('keydown', event => {
+            this.keydowns[event.key] = true
+        })
+        window.addEventListener('keyup', function(event){
+            self.keydowns[event.key] = false
+        })
+        this.init()
+    }
+    static instance(...args) {
+        this.i = this.i || new this(...args)
+        return this.i
+    }
+    drawImage(img) {
+        this.context.drawImage(img.texture, img.x, img.y)
+    }
+    // update
+    update() {
+        this.scene.update()
+    }
+    // draw
+    draw() {
+        this.scene.draw()
+    }
+    //
+    registerAction(key, callback) {
+        this.actions[key] = callback
+    }
+    runloop() {
+        log(window.fps)
+        // events
+        var actions = Object.keys(this.actions)
+        for (var i = 0; i < actions.length; i++) {
+            var key = actions[i]
+            if(this.keydowns[key]) {
+                // 如果按键被按下, 调用注册的 action
+                this.actions[key]()
+            }
+        }
+        // update
+        this.update()
+        // clear
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        // draw
+        this.draw()
+        // next run loop
+        var g = this
+        setTimeout(function(){
+            g.runloop()
+        }, 1000/window.fps)
+    }
+    textureByName(name) {
+        log('image by name', this.images)
+        var img = this.images[name]
+        // var image = {
+        //     w: img.width,
+        //     h: img.height,
+        //     image: img,
+        // }
+        return image
+    }
+    runWithScene(scene) {
+        var g = this
+        g.scene = scene
+        // 开始运行程序
+        setTimeout(function(){
+            g.runloop()
+        }, 1000/window.fps)
+    }
+    replaceScene(scene) {
+        this.scene = scene
+    }
+    __start(scene) {
+        this.runCallback(this)
+    }
+
+    init() {
+        var g = this
+        var loads = []
+        // 预先载入所有图片
+        var names = Object.keys(this.images)
+        for (var i = 0; i < names.length; i++) {
+            let name = names[i]
+            var path = this.images[name]
+            let img = new Image()
+            img.src = path
+            img.onload = function() {
+                // 存入 g.images 中
+                g.images[name] = img
+                // 所有图片都成功载入之后, 调用 run
+                loads.push(1)
+                log('load images', loads.length, names.length)
+                if (loads.length == names.length) {
+                    log('load images', g.images)
+                    g.__start()
+                }
+            }
+        }
+    }
+}
---
+++ scene/main/ball.js
@@ -0,0 +1,33 @@
+var Ball = function(game) {
+    var o = game.textureByName('ball')
+    // var image = imageFromPath('ball.png')
+    o.x = 100
+    o.y = 200
+    o.speedX = 5
+    o.speedY = 5
+    o.fired = false
+    o.fire = function() {
+        o.fired = true
+    }
+    o.move = function() {
+        if (o.fired === true) {
+            if (o.x < 0 || o.x > 500) {
+                o.speedX = -o.speedX
+            }
+            if (o.y < 0 || o.y > 400) {
+                o.speedY = -o.speedY
+            }
+            o.x += o.speedX
+            o.y += o.speedY
+        }
+    }
+    o.bounce = function() {
+        o.speedY *= -1
+    }
+    o.hasPoint = function(x, y) {
+        var xIn = x >= o.x && x <= o.x + o.w
+        var yIn = y >= o.y && y <= o.y + o.h
+        return xIn && yIn
+    }
+    return o
+}
